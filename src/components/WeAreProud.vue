e83d314eb533c1b26ceec48da88a1a2a0088c821
Clifford170 <cliffordbright107@gmail.com>
2024-05-15 10:55:59 +0200

    we are proud component

---
+++ src/components/WeAreProud.vue
@@ -0,0 +1,72 @@
+<script setup>
+
+</script>
+<template>
+<div class="container-fluid weAreContainer">
+<div class="row">
+    <div class="col-12 ">
+        <pre class="weAreProud">
+             We’re proud of our data and it’s what we can do with it to help businesses that really gets us excited.
+       
+            Our pioneering combination of data, technology and advanced analytics can help you unlock insights and take decisive actions in the
+            moments that matter, whether you’re planning a show-stopping marketing campaign, making responsible credit decisions or creating a 
+            business optimisation strategy.
+
+            Our global coverage and flexible technology mean we can scale to meet the needs of businesses of any size, anywhere in the world. Our
+                                   focus on innovation makes sure your business always has access to the tools you need to stay ahead.
+               
+        </pre>
+     </div>
+     <div class="col btnn">
+        <button class="buttin">
+            Get Started with Product Portal <i class="bi bi-arrow-up-right"></i>
+        </button>
+     </div>
+</div>
+</div>
+</template>
+
+
+<style>
+
+.weAreContainer{
+    width: 1920px;
+    min-height: 400px;
+    background: #1D4F91 0% 0% no-repeat padding-box;
+    opacity: 1;
+}
+.weAreProud{
+    display: flex;
+    position: relative;
+    top: 40px;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+    font: normal normal normal 14px/22px Roboto;
+    letter-spacing: 0px;
+    color: #FFFFFF;
+    opacity: 1;
+}
+.buttin{
+    align-self: center;
+    width: 397px;
+    height: 72px;
+    background: #FFFFFF 0% 0% no-repeat padding-box;
+    box-shadow: 0px 3px 6px #0000000D;
+    border: 1px solid #FFFFFF;
+    border-radius: 5px;
+    opacity: 1;
+    text-align: center;
+    font: normal normal bold 18px/25px Roboto;
+    letter-spacing: 0px;
+    color: #000000;
+    opacity: 1;
+
+}
+.btnn{
+    display: flex;
+    justify-content: center;
+}
+
+
+</style>
