23d521549f737978e22243e760e591390e86aed8
Clifford170 <cliffordbright107@gmail.com>
2024-05-15 10:50:23 +0200

    signup component

---
+++ src/components/SignupNow.vue
@@ -0,0 +1,42 @@
+<script setup>
+   
+    
+</script>
+<template>
+<div class="container-fluid containerss">
+<div class="row">
+    <div class="col-12 signup">
+        Sign Up Now and Take Your Business <br> to the <span><img src="/src/assets/Exprerian/DrivenWording – 1.png" alt=""></span> Level
+    </div>
+    <div class="col-12 innovative">
+        Innovative, user-friendly information solutions that you can trust
+    </div>
+</div>
+</div>
+</template>
+
+<style>
+.containerss{
+    min-height: 330px;
+    background: #ECF0F6 0% 0% no-repeat padding-box;
+    opacity: 1;
+}
+.signup{
+    width: 654px;
+    height: 86px;
+    text-align: center;
+    font: normal normal bold 40px/100px Roboto;
+    letter-spacing: 0px;
+    color: #6D2077;
+}
+.innovative{
+    position: relative;
+    top: 150px;
+    text-align: center;
+    font: normal normal bold 18px/30px Roboto;
+    letter-spacing: 0px;
+    color: #000000;
+    opacity: 1;
+
+}
+</style>
