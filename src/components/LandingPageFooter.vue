28ab0ebfc1b88a9ffe0607716ca5ca12059ea254
Clifford170 <cliffordbright107@gmail.com>
2024-05-15 10:08:14 +0200

    landingPage footer

---
+++ src/components/LandingPageFooter.vue
@@ -0,0 +1,51 @@
+
+<script setup>
+
+</script>
+<template>
+
+    <div class="container-fluid">
+        <div class="row footer">
+            <div class="col">
+                <img src="/src/assets/Exprerian/CompuloanLogo.png">
+            </div>
+            <div class="col">
+                <img src="/src/assets/Exprerian/ColumbusLogo.png">
+            </div>
+            <div class="col">
+                <img src="/src/assets/Exprerian/CodixLogo.png">
+            </div>
+            <div class="col">
+                <img src="/src/assets/Exprerian/XceleratorLogo.png">
+            </div>
+            <div class="col">
+                <img src="/src/assets/Exprerian/AASLogo.png">
+            </div>
+            <div class="col">
+                <img src="/src/assets/Exprerian/IncomePredictorLogo.png">
+            </div><div class="col">
+                <img src="/src/assets/Exprerian/BatchManagerLogo.png">
+            </div>
+            <div class="col">
+                <img src="/src/assets/Exprerian/AVSLogo.png">
+            </div>
+  </div>
+    </div>
+  
+</template>
+
+
+
+<style>
+
+.footer{
+    display: flex;
+    min-height: 131px;
+}
+.col{
+    position: relative;
+    top: 55px;
+    bottom: 48px;
+}
+
+</style>
