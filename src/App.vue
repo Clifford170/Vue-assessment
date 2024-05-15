3af9db7c1a81b6a0049c3934c3e5befb980f71fe
Clifford170 <cliffordbright107@gmail.com>
2024-05-15 13:43:41 +0200

    styling and app vue commits

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from "./components/LandingPage.vue";
+import LandingPageFooter from "./components/LandingPageFooter.vue"
+import FinancialInsights from "./components/FinancialInsights.vue"
+import Carousel from "./components/Carousel.vue"
+import Table from "./components/Table.vue"
+import SignupNow from "./components/SignupNow.vue"
+import WeAreProud from "./components/WeAreProud.vue"
+import bottomFooter from "./components/bottomFooter.vue"
+
 </script>
-
 <template>
-<div class="nav">
-hello vue
-</div>
+    <LandingPage/>
+    <LandingPageFooter />
+    <FinancialInsights />
+    <carousel />
+    <Table />
+    <SignupNow />
+    <WeAreProud />
+    <bottomFooter />
 </template>
-
-<style>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App).mount('#app')
