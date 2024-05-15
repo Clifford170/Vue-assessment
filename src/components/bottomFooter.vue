4f8290292c5f6a0d7574bb028e5dc9d64f5f06c5
Clifford170 <cliffordbright107@gmail.com>
2024-05-15 11:02:25 +0200

    bottom footer

---
+++ src/components/bottomFooter.vue
@@ -0,0 +1,68 @@
+<script>
+
+</script>
+<template>
+<div class="container-fuid btnContainer">
+<div class="row">
+        <div class="col-12 wording">
+            <span class="wordingMove">TERMS AND CONDITIONS</span>
+            <span class="wordingMove">PRIVACY POLICY</span>
+            <span class="wordingMove">PRESS</span>
+            <span class="wordingMove">CAREER</span>
+            <span class="wordingMove">INVESTOR RELATIONS</span>
+        </div>
+        <div class="col-12 moveLogo">
+            <span><img src="/src/assets/Exprerian/ExperianLogo.png" alt=""></span>
+        </div>
+</div>
+
+<div class="row lastwords">
+    <div class="col-12">
+        © 2022 Experian South Africa (Pty) Ltd. All Rights Reserved.
+    </div>
+    <div class="col-12">
+        Experian is a registered credit bureau in terms of the National Credit Act (NCA) Registration No: 2006/010440/07 l NCR No: NCRCB16
+    </div>
+</div>
+</div>
+</template>
+
+
+<style>
+
+.btnContainer{
+
+    min-height: 200px;
+    background: #FFFFFF 0% 0% no-repeat padding-box;
+    opacity: 1;
+    overflow-x: hidden;
+}
+.wording{
+    display: flex;
+    position: relative;
+    justify-content: end;
+    right: 70px;
+    top: 30px;
+
+}
+.wordingMove{
+    font: normal normal  bold 10px/15px Roboto;
+    letter-spacing: 0px;
+    color: #333333;
+    text-transform: uppercase;
+    padding-left: 15px;
+    opacity:1;
+}
+.moveLogo{
+    position: relative;
+    left: 50px;
+    margin-bottom: 50px;
+}
+.lastwords{
+    position: relative;
+    left: 50px;
+    margin-bottom: 50px;
+    font: normal normal  12px/15px Roboto;
+}
+
+</style>
